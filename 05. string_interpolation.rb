7500bad3ed5f5fd4cb3600a3aa3b0a372216cb32
Sagar Acharya <sagar_acharya@live.in>
2017-02-19 19:16:09 +0530

    string interpolation

---
+++ 05. string_interpolation.rb
@@ -0,0 +1,11 @@
+age = 25
+
+p "my current age is #{age}"
+
+p "addition: #{1 + 1}"
+
+p "after 5 years my age will be #{age + 5}"
+
+five_years = 5
+
+p "after 5 years my age will be #{age + five_years}"
