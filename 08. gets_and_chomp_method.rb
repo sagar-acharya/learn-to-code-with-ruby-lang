6748dc3e2e29831cea00383358fa7af1a146ca74
Sagar Acharya <sagar_acharya@live.in>
2017-02-19 20:21:19 +0530

    int & chomp, gets

---
+++ 08. gets_and_chomp_method.rb
@@ -0,0 +1,9 @@
+puts "What is your name?"
+puts "Enter Name:"
+name = gets.chomp
+
+puts "What is your age?"
+puts "Enter Age:"
+age = gets.chomp
+
+puts "Cool, so your name is #{name}, and your are #{age} years old."
