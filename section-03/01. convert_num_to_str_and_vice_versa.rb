ec0cdc66d43dcf630d15e31a83faaf9dbc317085
Sagar Acharya <sagar_acharya@live.in>
2017-02-19 23:58:05 +0530

    num, str, float conversion

---
+++ section-03/01. convert_num_to_str_and_vice_versa.rb
@@ -0,0 +1,29 @@
+#str to int and float
+str = "1"
+
+p str.class
+
+p str.to_i.class
+
+p str.to_f.class
+
+puts " "
+#int to str and float
+str = 1
+
+p str.class
+
+p str.to_s.class
+
+p str.to_f.class
+
+
+puts " "
+#float to int and str
+str = 3.14
+
+p str.class
+
+p str.to_s.class
+
+p str.to_i.class
